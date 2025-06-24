e02b774336e937edbd87436ac9065b74185f44a5
Chandana-Gajam <chandanagajam15@gmail.com>
2025-06-24 12:16:59 +0530

    Add files via upload

---
+++ Addnumbers.java
@@ -0,0 +1,12 @@
+public class Addnumbers{
+    public static void main(String[] args){
+
+        int num1 = 5;
+        int num2 = 4;
+
+        int result = num1 + num2;
+
+        System.out.println(result);
+
+    }
+}
